44d3d67748a47ef2293a1effe1436f523190c2ed
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-11-27 16:37:23 -0600

    Update eturtle.lua

--- API/eturtle.lua
+++ API/eturtle.lua
@@ -1,4 +1,4 @@
---[[V0.2.0
+--[[V0.3.0
 an API which stands for enhanced turtle, just creates more sophisticated methods
 --]]
 
@@ -10,8 +10,8 @@ function Eturtle:new()
 	Eturtle = {}
 	setmetatable(Eturtle, self)
 	self.__index = self
-	self.position = {} --x (+east, -west); y (+up, -down); z (+south, -north)
-	self.direction = "nil" --"north", "east", "south", "west"
+	self.position = vector.new(0,0,0) --x (+east, -west); y (+up, -down); z (+south, -north)
+	self.direction = "north" --"north", "east", "south", "west"
 	self.fuelLevel = turtle.getFuel()
 	self.fuelLimit = turtle.getFuelLimit()
 	self.contents = {} --{turtle.getItemDetail(1), turtle.getItemDetail(2), turtle.getItemDetail(3)...turtle.getItemDetail(16)}
@@ -34,7 +34,7 @@ end
 
 --[[
 Sets the direction of the Eturtle class to s
-NOTE: INTERNAL USE only
+NOTE: INTERNAL USE mostly
 If you are trying to turn the turtle to the north, east, etc
 use Eturtle:turnToAbsolute() instead.
 --params
@@ -96,30 +96,46 @@ end
 
 
 --[[
-Just a cooler way to turn the turtles, makes turning 180 a lot easier.
+Used for turtle initialization for any values which cannot already be determined.
+IE. set the turtles position/direction here,
+otherwise it will use relative positioning.
+(north = whichever way the turtle was facing, and not the actual world north)
+(0,0,0 = where the turtle started, and not the actual world 0,0,0)
+Recommended you use GPS for determining pos, orientation is set manually
+--]]
+function Eturtle:init(v, s)
+	self._setPosition(v)
+	self._setDirection(s)
+end
+
+
+--[[
+A better way to turn turtles.
+USE THIS FUNCTION OVER turtle.turnLeft() or turtle.turnRight()
+if you want to transmit the Eturtles info accurately.
 --params
 s1 = a string indicating where to turn to ("left", "right", "back") are the only accepted params
 --]]
 function Eturtle:turnTo(s)
 	if s == "left" then
 		if turtle.turnLeft() then 
-			Eturtle:_incrementDirection(-1)
+			self:_incrementDirection(-1)
 		else
 			error("eturtle turnTo ecode:0") 
 		end
 	elseif s == "back" then
 		if turtle.turnLeft() then 
 			turtle.turnLeft() 
-			Eturtle:_incrementDirection(-2)
+			self:_incrementDirection(-2)
 		elseif turtle.turnRight() then 
 			turtle.turnRight() 
-			Eturtle:_incrementDirection(2)
+			self:_incrementDirection(2)
 		else
 			error("eturtle turnTo ecode:1") 
 		end
 	elseif s == "right" then
 		if turtle.turnRight() then 
-			Eturtle:_incrementDirection(1)
+			self:_incrementDirection(1)
 		else
 			error("eturtle turnTo ecode:2")
 		end
@@ -135,32 +151,107 @@ Turns to an absolute position given
 s = string ("north", "east", "south", "west")
 --]]
 function Eturtle:turnToAbsolute(s)
-	keyLeft = 
+	local keyLeft = 
 		{["northnorth"] = 0, ["northeast"] = 3, ["northsouth"] = 2, ["northwest"] = 1
 		["eastnorth"] = 1, ["easteast"] = 0, ["eastsouth"] = 3, ["eastwest"] = 2
 		["southnorth"] = 2, ["southeast"] = 1, ["southsouth"] = 0, ["southwest"] = 3
 		["westnorth"] = 3, ["westeast"] = 2, ["westsouth"] = 1, ["westwest"] = 0
 		}
-	keyRight =
+	local keyRight =
 		{["northnorth"] = 0, ["northeast"] = 1, ["northsouth"] = 2, ["northwest"] = 3
 		["eastnorth"] = 3, ["easteast"] = 0, ["eastsouth"] = 1, ["eastwest"] = 2
 		["southnorth"] = 2, ["southeast"] = 3, ["southsouth"] = 0, ["southwest"] = 1
 		["westnorth"] = 1, ["westeast"] = 2, ["westsouth"] = 3, ["westwest"] = 0
 		}
-	leftTurnCount = keyLeft[(self.direction)..s]
-	rightTurnCount = keyRight[(self.direction)..s]
+	local leftTurnCount = keyLeft[(self.direction)..s]
+	local rightTurnCount = keyRight[(self.direction)..s]
 	if leftTurnCount <= rightTurnCount then
 		for i=1, leftTurnCount()
 			turtle.turnLeft()
-			Eturtle:_incrementDirection(-1)
+			self:_incrementDirection(-1)
 		end
 	else
 		for i=1, rightTurnCount then
 			turtle.turnRight()
-			Eturtle:_incrementDirection(1)
+			self:_incrementDirection(1)
 		end
 	end
 end
+
+
+--[[
+A better way to move turtles, it will only
+turn if absolutely necessary
+USE THIS FUNCTION over turtle.forward, et cetera
+if you want to transmit Eturtles info accurately.
+--params
+s = direction (string: "front", "back",
+"left", "right", "top", "bottom")
+n = amount to move that direction
+e = end direction (will end up facing same way if no input) ("left", "right", "back")
+--]]
+function Eturtle:moveDirection(s, n, e)
+	local a = 1
+	if n then
+		a = n
+	end
+	local turnKey = {["left"] = {true,"right"}, ["right"] = {true,"left"}}
+	--starting direction --> ending direction
+	local keyRelative = 
+		{["leftleft"] = "front",["leftright"] = "back",["leftback"] = "left",
+		["rightleft"] = "back",["rightright"] = "front",["rightback"] = "right",
+		["backleft"] = "left",["backright"] = "right",["backback"] = "back"
+		}
+	--creates a vector which chooses the appropriate direction
+	local positionKey =
+		{["north"] = vector.new(0,0,-1),
+		["east"] = vector.new(1,0,0),
+		["south"] = vector.new(0,0,1),
+		["west"] = vector.new(-1,0,0)
+		}
+	if key[s][1] then
+		self:turnTo(s)
+		for i=1, a do
+			if not turtle.forward() then break end
+			self._setPosition(self.position + positionKey[self.direction])
+		end
+	elseif s == "front"
+		for i=1, a do
+			if not turtle.back() then break end
+			self._setPosition(self.position + positionKey[self.direction])
+		end
+	elseif s == "back"
+		for i=1, a do
+			if not turtle.back() then break end
+			self._setPosition(self.position + positionKey[self.direction])
+		end
+	elseif s == "up"
+		for i=1, a do
+			if not turtle.up() then break end
+			self._setPosition(self.position + vector.new(0,1,0))
+		end
+	elseif s == "down"
+		for i=1, a do
+			if not turtle.down() then break end
+			self._setPosition(self.position + vector.new(0,-1,0))
+		end
+	end
+	local eMod
+	if e then
+		eMod = keyRelative[s..e]
+	end
+	if not e and key[s][1] then
+		self.turnTo(key[s][2])
+	elseif eMod then
+		self.turnTo(eMod)
+	end
+end
+
+
+--[[
+todo
+--]]
+
 
 
 --[[temp disabled (has no use, and is confusing rn)
