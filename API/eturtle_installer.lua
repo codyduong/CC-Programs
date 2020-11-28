c75e17a2527f5f9905e4e869968fa0e225501182
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-11-28 01:08:23 -0600

    Update eturtle_installer.lua

--- API/eturtle_installer.lua
+++ API/eturtle_installer.lua
@@ -1,12 +1,12 @@
---[[v0.0.6
+--[[v0.0.7
 self-referencial pastebin here: rPpJjEFx
 Installs the necessary components for eturtle in one package
 Currently contains:
 startup.lua (V0.0.2) [If not already installed]
 
-eturtle.lua (V0.3.12)
+eturtle.lua (V0.3.14)
 json.lua --stored constantly at 4nRg9CHU (hopefully)
-eturtle_startup.lua (V0.0.4)
+eturtle_startup.lua (V0.0.6)
 --]]
 
 if not fs.exists("api") then fs.makeDir("api") end
