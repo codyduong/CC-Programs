91f4da7e9151a4a2216f7e4c5243ab0be72d9e1c
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-11-28 09:47:08 -0600

    updater implementation

--- API/eturtle_installer.lua
+++ API/eturtle_installer.lua
@@ -1,32 +1,48 @@
---[[v0.0.9
+--[[v0.0.10
 self-referencial pastebin here: rPpJjEFx
 Installs the necessary components for eturtle in one package
+also planned to update files if necessary
 Currently contains:
 startup.lua (V0.0.2) [If not already installed]
 
 eturtle.lua (V0.3.18)
-json.lua --stored constantly at 4nRg9CHU (hopefully)
 eturtle_startup.lua (V0.0.7)
+--params
+arg1 = boolean stating whether to update (optional parameter)
+
+EX: 
+eturtle.installer true --> will replace old files with new ones
+eturtle.installer --> will only install files that are missing
 --]]
+local arg1, extra = ...
 
 if not fs.exists("api") then fs.makeDir("api") end
 if not fs.exists("startdir") then fs.makeDir("startdir") end
-if not fs.exists("startup") then shell.run("pastebin get 7EtT3zBn startup.lua") end
-if not fs.exists("api/eturtle.lua") then
-	shell.run("pastebin get 5emHRrcV eturtle.lua")
-	shell.run("move eturtle.lua api/")
-end
---[[
-if not fs.exists("api/json.lua") then
-	shell.run("pastebin get 4nRg9CHU json.lua")
-	shell.run("move json.lua api/")
-end
---]]
-if not fs.exists("startdir/eturtle_startup.lua") then
-	shell.run("pastebin get mNVUrxpn eturtle_startup.lua")
-	shell.run("move eturtle_startup.lua startdir/")
+if not arg1 then --If not asking for a reinstall, do a clean install: does not overwrite any files.
+	if not fs.exists("startup") then shell.run("pastebin get 7EtT3zBn startup.lua") end
+	if not fs.exists("api/eturtle.lua") then
+		shell.run("pastebin get 5emHRrcV eturtle.lua")
+		shell.run("move eturtle.lua api/")
+	end
+	if not fs.exists("startdir/eturtle_startup.lua") then
+		shell.run("pastebin get mNVUrxpn eturtle_startup.lua")
+		shell.run("move eturtle_startup.lua startdir/")
+	end
+	print("Installed and rebooting")
+	os.sleep(3) --just so the user can read the prompt
+	os.reboot()
+else 
+	if not fs.exists("startup") then shell.run("pastebin get 7EtT3zBn startup.lua") end
+	if not fs.exists("api/eturtle.lua") then
+		shell.run("pastebin get 5emHRrcV eturtle.lua")
+		shell.run("move eturtle.lua api/")
+	end
+	if not fs.exists("startdir/eturtle_startup.lua") then
+		shell.run("pastebin get mNVUrxpn eturtle_startup.lua")
+		shell.run("move eturtle_startup.lua startdir/")
+	end
+	print("Updated and rebooting")
+	os.sleep(3) --just so the user can read the prompt
+	os.reboot()
 end
 
-print("Installed and rebooting")
-os.sleep(3)
-os.reboot()
